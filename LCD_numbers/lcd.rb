57154f33c9ab07415f08e7655c3db5567a30d52f
Jingjing Duan <duanjingjing@gmail.com>
2012-04-06 22:49:09 -0700

    LCD numbers: first working version

--- LCD_numbers/lcd.rb
+++ LCD_numbers/lcd.rb
@@ -1,12 +1,45 @@
 #!/usr/bin/env ruby
 #
-nums = {
+shapes = {
+  0 => [" - ", "| |", "   ", "| |", " - "],
+  1 => ["   ", "  |", "   ", "  |", "   "],
+  2 => [" - ", "  |", " - ", "|  ", " - "],
+  3 => [" - ", "  |", " - ", "  |", " - "],
+  4 => ["   ", "| |", " - ", "  |", "   "],
   5 => [" - ", "|  ", " - ", "  |", " - "],
   6 => [" - ", "|  ", " - ", "| |", " - "],
+  7 => [" - ", "  |", "   ", "  |", "   "],
+  8 => [" - ", "| |", " - ", "| |", " - "],
+  9 => [" - ", "| |", " - ", "  |", " - "],
 }
 
-0.upto(4) do |i|
-  sum = nums.values.inject() do |sum, line|
+def scale(shape, size)
+  shape.each do |line|
+    line[1,1] *= size
+  end
+  new_shape = []
+  shape.each do |line|
+    if line.include? '|'
+      size.times { new_shape << line }
+    else 
+      new_shape << line
+    end
+  end
+  new_shape
+end
+
+num = ARGV[0]
+size = ARGV[1].to_i
+
+prints = []
+num.each_char do |digit|
+  prints << scale(shapes[digit.to_i], size)
+end
+
+vertical_size = prints[0].size
+
+0.upto(vertical_size - 1) do |i|
+  sum = prints.inject("") do |sum, line|
     sum += line[i]
     sum += " "
   end
